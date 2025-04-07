5644ca200f3ab38771e539929cc12191a9b6de78
Andi Muhammad Raihan Syamsu <amuhraihan2324@gmail.com>
2025-04-07 12:32:45 +0800

    Memisahkan Composable dari Activity

--- app/src/main/java/com/andimuhammadraihansyamsu607062330113/asesment1/MainActivity.kt
+++ app/src/main/java/com/andimuhammadraihansyamsu607062330113/asesment1/MainActivity.kt
@@ -4,21 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.andimuhammadraihansyamsu607062330113.asesment1.ui.screen.MainScreen
 import com.andimuhammadraihansyamsu607062330113.asesment1.ui.theme.Asesment1Theme
 
 class MainActivity : ComponentActivity() {
@@ -33,43 +19,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ) { innerPadding ->
-        ScreenContent(modifier = Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "${stringResource(id = R.string.welcome_message)} ${stringResource(id = R.string.app_name)}!")
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Asesment1Theme {
-        MainScreen()
-    }
-}
---
+++ app/src/main/java/com/andimuhammadraihansyamsu607062330113/asesment1/ui/screen/MainScreen.kt
@@ -0,0 +1,60 @@
+package com.andimuhammadraihansyamsu607062330113.asesment1.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.andimuhammadraihansyamsu607062330113.asesment1.R
+import com.andimuhammadraihansyamsu607062330113.asesment1.ui.theme.Asesment1Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "${stringResource(id = R.string.welcome_message)} ${stringResource(id = R.string.app_name)}!")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Asesment1Theme {
+        MainScreen()
+    }
+}
